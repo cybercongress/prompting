3953b1a6e18ee59b2ccce221bbe9777f7d4cc196
cyborgshead <cyborgshead@proton.me>
2024-07-11 13:04:46 +0800

    Fixed set_weights call

--- prompting/base/validator.py
+++ prompting/base/validator.py
@@ -272,7 +272,6 @@ class BaseValidatorNeuron(BaseNeuron):
             uids=uint_uids,
             weights=uint_weights,
             wait_for_finalization=False,
-            wait_for_inclusion=False,
             version_key=self.spec_version,
         )
         if result is True:
